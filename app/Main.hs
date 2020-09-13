1fa6963955682d62e663fff8946068601f762803
Ben <ben@Bens-iMac-Pro.local>
2020-09-13 14:35:34 -0400

    Got simultaneous reflected paths working

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ import Control.Applicative
 import Control.Monad
 import Test.QuickCheck.Gen
 import Test.QuickCheck.Random
+import Data.List
 
 type Point = (Double, Double)
 data Line = Line { slope :: Double, intercept :: Double }
@@ -23,8 +24,8 @@ reflectPoint (px, py) (Line m b) = (x, y)
         x = ((1 - m*m)*px + 2*m*py - 2*m*b) / (m*m + 1)
         y = ((m*m - 1)*py + 2*m*px + 2*b) / (m*m + 1)
 
-reflectLine :: (Point, Point) -> Line -> (Point, Point)
-reflectLine (p1, p2) line = (reflectPoint p1 line, reflectPoint p2 line)
+reflectPath :: [Point] -> Line -> [Point]
+reflectPath path line = map (flip reflectPoint line) path
 
 
 evenRadialPoints :: Int -> [Point]
@@ -45,22 +46,20 @@ randomPoint bounds = liftM2 (,) (choose bounds) (choose bounds)
 randomPoints :: Int -> (Double, Double) -> [Point]
 randomPoints n bounds = unGen (vectorOf n $ randomPoint bounds) (mkQCGen 1) n
 
-pairs :: [a] -> [(a, a)]
-pairs [] = []
-pairs [x] = [(x, x)]
-pairs (x1:x2:xs) = [(x1, x2)] ++ pairs xs
-
+chunk :: Int -> [a] -> [[a]]
+chunk n [] = []
+chunk n xs = [take n xs] ++ (chunk n $ drop n xs)
 
 main :: IO ()
 main = reanimate $ docEnv $ pauseAtEnd 3 $ mkAnimation 5 $ \t ->
     partialSvg t $ pathify $ mkGroup $
-    map (uncurry mkLine) (reflectedLines ++ unreflectedLines)
+    map mkLinePath (reflectedPath ++ unreflectedPath)
     where
-        radialPoints = evenRadialPoints 9
+        radialPoints = evenRadialPoints 5
         reflectedPoints = map (\(x, y) -> (-x, -y)) radialPoints
-        lines = map (uncurry mkLn) $ zip radialPoints reflectedPoints
-        randomLines = pairs $ randomPoints 8 (0.5, 4.0)
-        -- randomLines = [((2.0, 3.3), (1.1, 2.6)), ((1.4, 4.7), (1.6, 1.1))]
-        reflectedLines = liftM2 (reflectLine) randomLines lines -- concatMap (map (flip reflectLine) lines) randomLines
-        unreflectedLines = liftM2 (reflectLine) reflectedLines lines
+        -- lines = map (uncurry mkLn) $ zip radialPoints reflectedPoints
+        lines = defineRadialSymmetries 5
+        randomPath = randomPoints 8 (0.5, 4.0)
+        reflectedPath = map (reflectPath randomPath) lines
+        unreflectedPath = map (reflectPath (head reflectedPath)) lines
         
